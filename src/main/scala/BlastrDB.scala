9de19f987b5b64b271745c3f91929273154a1a08
Cameron <camerondev343@gmail.com>
2020-12-11 14:49:13 -0500

    Rewrote to write data to file instead if printing

--- src/main/scala/BlastrDB.scala
+++ src/main/scala/BlastrDB.scala
@@ -1,6 +1,8 @@
 package csv
 
 import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
 
 /** BlastrDB
   * pulls data from the csv-files folder and parses them into a formatted list.
@@ -8,20 +10,24 @@ import java.io.File
 object BlastrDB extends App {
   println("Brand\t\t|\tBlaster Name")
   val folder = getListOfFiles("./csv-files")
+  val bufferFile = new File("compiled-list.txt")
+  val bw = new BufferedWriter(new FileWriter(bufferFile))
+  bw.write("Brand\t\t|\tBlaster Name\n")
   for (ftchfile <- folder) {
     val file = io.Source.fromFile(ftchfile)
     for (line <- file.getLines) {
-      println(
-        ftchfile
+      bw.append(
+        (ftchfile
           .getAbsolutePath()
           .substring(
             // this line formats the path to display the file name as the brand
             ftchfile.getAbsolutePath().lastIndexOf("\\") + 1,
             ftchfile.getAbsolutePath().length() - 4
-          ) + " | " + line
-      ) //TODO: replace println with an output to file
+          ) + " | " + line + "\n").toString()
+      )
     } //TODO: Replace for loops with futures call
   }
+  bw.close()
 
   /** getListOfFiles
     * helper method that populates a list of files drawn from a specified folder
