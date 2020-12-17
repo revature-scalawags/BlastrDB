4c31054b64844815acddc34eeb70209421ed4998
Cameron <camerondev343@gmail.com>
2020-12-17 11:09:34 -0500

    Implemented CLI

--- src/main/scala/BlastrDB.scala
+++ src/main/scala/BlastrDB.scala
@@ -18,84 +18,77 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
 
-
-
-/*mongoDB Hooks
-  val client = MongoClient()
-*/
-
 /** BlastrDB
   * pulls data from a formatted website link and parses them into formatted lists.
   *
-  * @version 0.13
+  * @version 0.19
   * @todo add runtime CLI menu to allow for user interaction
   * @todo add hooks and ouput for mongoDB
   * @todo add scalatest items
   */
 object BlastrDB extends App {
-  println("BlastrDB starting...")
-  println(
-    "BlastrDB is parsing and adding data from listed websites now.\n\t" +
-      " If you would like to add, remove, or change a website\n\t " +
-      "to add data from, you may do so in the \"dataPullParse.csv\" file."
-  )
-  //println("Welcome to BlastrDB, please enter one of the options below:")
-  //TODO: refactor into CLI
+  println("\nBlastrDB starting...")
+
   val loggerFile = new File("debugLog.txt")
   val debugFileBuffer = new BufferedWriter(new FileWriter(loggerFile))
   debugFileBuffer.write("Debug Log start:\n")
 
   //database hooks
-  
+  debugFileBuffer.write("Connecting to MongoDB...\n")
   val mongoClient: MongoClient = MongoClient()
   val database: MongoDatabase = mongoClient.getDatabase("testdb")
   val collection = database.getCollection("name").find()
-  /* for(i <- collection) {
-    println(i("Brand").asString.getValue + " " + i("Name").asString.getValue)
-  } */ //this 'for' loop prints out the db collection as a formatted list of strings
+  //for(i <- collection) {
+  //  println(i("Brand").asString.getValue + " " + i("Name").asString.getValue)
+  //} //this 'for' loop prints out the db collection as a formatted list of strings
+  debugFileBuffer.write("MongoDB successfully connected.\n")
 
-  val dataPullSource = io.Source.fromFile("dataPullParse.csv")
-  for (line <- dataPullSource.getLines) {
-    val cols = line.split(",").map(_.trim)
-    debugFileBuffer.write("Pulling data from " + cols(0) + "... \n")
-    try {
-      pullData(cols(0), cols(1), cols(2), cols(3).toBoolean)
-    } catch {
-      case e: IllegalArgumentException =>
-        println(
-          "!!!Incorrect formatting found in dataPullParse.csv, check debug.txt for details!!!"
-        )
-        debugFileBuffer.write(getStackTraceAsString(e))
-
+  //CLI
+  var userInput = ""
+  println(
+    "Welcome to BlastrDB, the following options below are avilable to you:\n" +
+      "\tpull:\tPulls the data from the list of websites.\n" +
+      "\twrite:\tWrites the data to seperate CSV files based on brand.\n" +
+      "\texit:\tExits the program."
+  )
+  while (userInput != "exit") {
+    println("Please enter a command: ")
+    userInput = scala.io.StdIn.readLine()
+    debugFileBuffer.write(s"user input: '$userInput'\n")
+    userInput match {
+      case "pull" => dataWriteToFile(debugFileBuffer)
+      case "write" => writeToBrandFiles(debugFileBuffer)
+      case "exit" => println("Exiting program...")
     }
   }
-  dataPullSource.close
 
   //TODO: pull data from a 3rd party website
-  debugFileBuffer.write(
-    "Data pull complete, writing data to aggregate csv file \"compiled-list.csv\"...\n"
-  )
-  val folder = getListOfFiles("./csv-files")
-  val bufferFile = new File("compiled-list.csv")
-  val bw = new BufferedWriter(new FileWriter(bufferFile))
-  //compiled-list.csv is organized by Brand,Name
-  for (ftchfile <- folder) {
-    val file = io.Source.fromFile(ftchfile)
-    for (line <- file.getLines) {
-      bw.append(
-        (ftchfile
-          .getAbsolutePath()
-          .substring(
-            // this line formats the path to display the file name as the brand
-            ftchfile.getAbsolutePath().lastIndexOf("\\") + 1,
-            ftchfile.getAbsolutePath().length() - 4
-          ) + "," + line + "\n").toString()
-      )
+
+  def writeToBrandFiles(debugFile: BufferedWriter) {
+    println("Writing data to seperate CSV files based on brand...")
+    val folder = getListOfFiles("./csv-files")
+    val bufferFile = new File("compiled-list.csv")
+    val bw = new BufferedWriter(new FileWriter(bufferFile))
+    //compiled-list.csv is organized by Brand,Name
+    for (ftchfile <- folder) {
+      val file = io.Source.fromFile(ftchfile)
+      for (line <- file.getLines) {
+        bw.append(
+          (ftchfile
+            .getAbsolutePath()
+            .substring(
+              // this line formats the path to display the file name as the brand
+              ftchfile.getAbsolutePath().lastIndexOf("\\") + 1,
+              ftchfile.getAbsolutePath().length() - 4
+            ) + "," + line + "\n").toString()
+        )
+      }
     }
+    debugFileBuffer.write("Data write to files complete, closing buffer...\n")
+    bw.close()
+    debugFileBuffer.write("buffer closed.\n")
+    println("Data write to files complete.")
   }
-  debugFileBuffer.write("Data write to files complete, closing buffer...\n")
-  bw.close()
-  debugFileBuffer.write("buffer closed.\n")
 
   /** getListOfFiles
     * populates a list of files drawn from a specified folder
@@ -179,9 +172,40 @@ object BlastrDB extends App {
     sw.toString
   }
 
+  /** dataWriteToFile
+    * pulls the data from the dataPullParse.csv file and writes it into
+    * an aggregate csv file titled 'compiled-list.csv' utilizing the 'pullData'function
+    *
+    * @param fileBuffer the debug file
+    */
+  def dataWriteToFile(fileBuffer: BufferedWriter) = {
+    debugFileBuffer.write("Beginning data pull from websites...\n")
+    println("Beginning Data pull from websites...")
+    val dataPullSource = io.Source.fromFile("dataPullParse.csv")
+    for (line <- dataPullSource.getLines) {
+      val cols = line.split(",").map(_.trim)
+      debugFileBuffer.write("Pulling data from " + cols(0) + "... \n")
+      try {
+        pullData(cols(0), cols(1), cols(2), cols(3).toBoolean)
+      } catch {
+        case e: IllegalArgumentException =>
+          println(
+            "!!!Incorrect formatting found in dataPullParse.csv, check debug.txt for details!!!"
+          )
+          debugFileBuffer.write(getStackTraceAsString(e))
+
+      }
+    }
+    println("Data pull complete.")
+    debugFileBuffer.write(
+      "Data pull complete, writing data to aggregate csv file \"compiled-list.csv\"...\n"
+    )
+    dataPullSource.close
+  }
+
   debugFileBuffer.write("debug log complete.")
   debugFileBuffer.close()
   println(
-    "BlastrDB processes complete, check debugLog.txt for a detailed runtime log."
+    "BlastrDB closed, check debugLog.txt for a detailed runtime log."
   )
 }
